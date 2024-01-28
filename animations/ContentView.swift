26f4ede212903681d47fa5554ffd5dee15ea3691
Jigmet Dadul <85403349+jigmetdadul@users.noreply.github.com>
2024-01-28 19:36:26 +0530

    Initial Commit

---
+++ animations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  animations
+//
+//  Created by Jigmet stanzin Dadul on 28/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ animations/animationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationsApp.swift
+//  animations
+//
+//  Created by Jigmet stanzin Dadul on 28/01/24.
+//
+
+import SwiftUI
+
+@main
+struct animationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
